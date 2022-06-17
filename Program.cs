04c92180f0d5790eeecc5bac6c23317ca88adac7
«Dmitrii <bimas2006@mail.ru>
2022-06-17 21:57:12 +0300

    v

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿static void SelectArr()
+{
+    string[] arr1 = new string[] { "колобок", "уж", "ёрш", "гипотоламус", "кукушка", "мышь", "lan", "15" };
+    int[] array = new int[arr1.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            Console.WriteLine(arr1[i]);
+        }
+
+    }
+
+
+}
+SelectArr();
