a7c531948f8918a6f62058108a7aac355e75fbfb
«Dmitrii <bimas2006@mail.ru>
2022-06-19 22:42:29 +0300

    v_3

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿static void SelectArr()
 {
-    string[] array = new string[] { "колобок", "уж", "ёрш", "гипотоламус", "кукушка", "мышь", "lan", "15" };
+    string[] array = new string[] { "колобок", "уж", "ёрш", "гипотоламус", "кукушка", "мышь", "lan", "15", ":-)" };
     int z = 0;
     for (int i = 0; i < array.Length; i++)
     {
@@ -19,9 +19,11 @@
             z++;
         }
     }
+    Console.Write("[");
     for (int j = 0; j < array2.Length; j++)
     {
-        Console.Write(array2[j] + "; ");
+        Console.Write("'"+ array2[j] +"'"+ ", ");
     }
+    Console.Write("]");
 }
 SelectArr();
